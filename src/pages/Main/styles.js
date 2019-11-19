6e1137c468794eb89d90f4ec79554827e3941fb7
evertondg <evertondg@hotmail.com.br>
2019-11-19 00:55:50 -0300

    chore: Ajustes de posicionamento do conteudo da aplicacao

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,12 @@
 import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 700px;
+  /*  max-width: 600px;*/
   background: #fff;
   border-radius: 4px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
   padding: 30px;
-  margin: 80px auto;
+  margin: 20px 25px;
 
   h1 {
     font-size: 20px;
